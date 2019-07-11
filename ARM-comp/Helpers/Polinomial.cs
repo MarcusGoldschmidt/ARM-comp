612bc7550c4e1dbf5052d349fe8a00a34ba3ec60
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-07-10 21:00:44 -0400

    Add soma de polinomios

--- ARM-comp/Helpers/Polinomial.cs
+++ ARM-comp/Helpers/Polinomial.cs
@@ -28,16 +28,25 @@ namespace ARM_comp.Helpers.NotEval
                     if (newPolinomio.ContainsKey(potenciaFinal))
                         newPolinomio[potenciaFinal] += data.Value * paransData.Value;
                     else
-                    {
                         newPolinomio.Add(
                             data.Key + paransData.Key,
                             data.Value * paransData.Value
                         );
-                    }
                 }
             }
 
             Polinomio = newPolinomio;
+        }
+
+        public void Somar(Polinomial parans)
+        {
+            foreach (var (key, value) in parans.Polinomio)
+            {
+                if (Polinomio.ContainsKey(key))
+                    Polinomio[key] += value;    
+                else
+                    Polinomio.Add(key, value);
+            }
         }
     }
 }
--- ARM-comp/Tests/Unit/PolinomialTest.cs
+++ ARM-comp/Tests/Unit/PolinomialTest.cs
@@ -21,11 +21,34 @@ namespace ARM_comp.Tests.Unit
             new double[] {1},
             new double[] {1, 2}
         )]
-        public void BlocoParentesesTest(double[] actualFist, double[] actualSecond, double[] expected)
+        public void MultiplicarTest(double[] actualFist, double[] actualSecond, double[] expected)
         {
             var a = new Polinomial(actualFist.ToList());
             var b = new Polinomial(actualSecond.ToList());
             a.Multiplicar(b);
+            Assert.IsTrue(expected.SequenceEqual(a.Polinomio.Values.ToList()));
+        }
+
+        [TestCase(
+            new double[] {1, 1},
+            new double[] {1, 1},
+            new double[] {2, 2}
+        )]
+        [TestCase(
+            new double[] {1, 4, 8},
+            new double[] {1, 2},
+            new double[] {2, 6, 8}
+        )]
+        [TestCase(
+            new double[] {1},
+            new double[] {8, 5, 3, 9},
+            new double[] {9, 5, 3, 9}
+        )]
+        public void SomaTest(double[] actualFist, double[] actualSecond, double[] expected)
+        {
+            var a = new Polinomial(actualFist.ToList());
+            var b = new Polinomial(actualSecond.ToList());
+            a.Somar(b);
             Assert.IsTrue(expected.SequenceEqual(a.Polinomio.Values.ToList()));
         }
     }
