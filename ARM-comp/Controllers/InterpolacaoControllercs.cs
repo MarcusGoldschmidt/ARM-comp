64ebd7e61b7014750532395df16ef7b965f32f54
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-07-10 22:47:28 -0400

    [Add] Dto e controller funcional

---
+++ ARM-comp/Controllers/InterpolacaoControllercs.cs
@@ -0,0 +1,22 @@
+﻿using ARM_comp.Models.Interpolacao.Metodos;
+using ARM_comp.Models.Metodos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ARM_comp.Controllers
+{
+    [Route("api/interpolacao")]
+    [ApiController]
+    public class InterpolacaoController : ControllerBase
+    {
+        [HttpPost("lagrange")]
+        public ActionResult<string> Bissecao([FromBody] LagrangeDto value)
+        {
+            var json = new
+            {
+                result = new Lagrange(value).Interpolacao()
+            };
+            return JsonConvert.SerializeObject(json);
+        }
+    }
+}
---
+++ ARM-comp/Models/Interpolacao/Metodos/LagrangeDto.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using ARM_comp.Models.Interpolacao;
+
+namespace ARM_comp.Models.Metodos
+{
+    public class LagrangeDto
+    {
+        public List<PontoCartesiano> Pontos { get; set; }
+    }
+}
